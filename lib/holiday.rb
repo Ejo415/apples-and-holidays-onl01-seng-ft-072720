53b2212ed65bdcdf99c296880f6ff257e354da18
Eugene O'Neil <ejo7220@gmail.com>
2020-07-31 23:40:36 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -48,8 +48,8 @@ end
 def all_winter_holiday_supplies(holiday_hash)
   # return an array of all of the supplies that are used in the winter season
   winter = []
-  holiday_hash[:winter].values = supplies
-  winter << supplies
+  winter << holiday_hash[:winter].value
+  
 winter
 end
 
