0b439ea0f4e37acf30fe5e357054943910640046
Eugene O'Neil <ejo7220@gmail.com>
2020-08-01 00:04:41 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -65,7 +65,8 @@ def all_supplies_in_holidays(holiday_hash)
     puts "#{season.capatalize}:"
     holiday.each do |holiday, supplies|
       puts " #{holiday.to_s.split(" ").map{|i| i.capatalize.join(" ")} #{supplies.join(", ")}"
-
+    end
+end
 end
 end
 
